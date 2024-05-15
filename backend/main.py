dcc3cb3a328a6918d1d9a60d400d335d7c4449aa
Ethan Cheng <ethan@nijika.org>
2024-05-14 19:26:29 -0700

    implement /user/delete

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,27 @@ from flask import Flask, jsonify, request, abort
 import Database
 
 app = Flask(__name__)
+db = Database.db()
+db.init()
 
 @app.route("/user/create", methods=["POST"])
 def create_user():
+    global db;
+
     try:
         data = request.get_json()
     except:
         return jsonify({"error": "invalid json"}), 400
+
+    try:
+        data["username"]
+    except:
+        return jsonify({"error": "missing username"}), 400
+
+    try:
+        data["password"]
+    except:
+        return jsonify({"error": "missing password"}), 400
 
     try:
         db.create_user(data["username"], data["password"])
@@ -17,9 +31,31 @@ def create_user():
 
     return "", 200
 
+@app.route("/user/delete", methods=["POST"])
+def delete_user():
+    global db;
+
+    try:
+        data = request.get_json()
+    except:
+        return jsonify({"error": "invalid json"}), 400
+
+    try:
+        data["username"]
+    except:
+        return jsonify({"error": "missing username"}), 400
+
+    try:
+        data["password"]
+    except:
+        return jsonify({"error": "missing password"}), 400
+
+    try:
+        db.delete_user(data["username"], data["password"])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    return "", 200
 
 if __name__ == "__main__":
-    global db
-    db = Database.db()
-    db.init()
     app.run(debug=True)

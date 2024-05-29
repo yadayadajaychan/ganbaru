473e7ecca8495969ee9f984ac4545851d9d48fd0
2005benchen <2005benchen@gmail.com>
2024-05-28 20:05:25 -0700

    Added NavBar

--- frontend/components/nav/nav.tsx
+++ frontend/components/nav/nav.tsx
@@ -6,27 +6,31 @@ import NextLink from 'next/link';
 
 export default function Nav() {
   return (
-    <Flex className='flex flex-row gap-2'>
-      <NextLink
-        href='/'
-        className='roup flex select-none items-center justify-betwee gap-[2px] rounded-[4px] px-3 py-2'
-      >
-        <Text size='2'>Home</Text>
-      </NextLink>
-      <HoverCard.Root>
-        <HoverCard.Trigger className='hover:bg-purple-300 hover:cursor-pointer hover:bg-opacity-10 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-2 py-2'>
-          <Flex>
-            <Text size='2'>Classes</Text>
-            <CaretDownIcon
-              className='relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
-              aria-hidden
-            />
-          </Flex>
-        </HoverCard.Trigger>
-        <HoverCard.Content>
-          <Text>test</Text>
-        </HoverCard.Content>
-      </HoverCard.Root>
+    <Flex className='flex flex-row justify-between items-center p-4'>
+      <Flex className='flex flex-row gap-2'>
+        <NextLink
+          href='/'
+          className='group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2'
+        >
+          <Text size='2'>Home</Text>
+        </NextLink>
+        <HoverCard.Root>
+          <HoverCard.Trigger className='hover:bg-purple-300 hover:cursor-pointer hover:bg-opacity-10 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-2 py-2'>
+            <Flex>
+              <Text size='2'>Classes</Text>
+              <CaretDownIcon
+                className='relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
+                aria-hidden
+              />
+            </Flex>
+          </HoverCard.Trigger>
+          <HoverCard.Content>
+            <Text>test</Text>
+          </HoverCard.Content>
+        </HoverCard.Root>
+      </Flex>
+
+
     </Flex>
   );
 }

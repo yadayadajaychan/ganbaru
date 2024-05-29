ac18c2d24a83320c4cafbf3188ae49c6300326d4
2005benchen <2005benchen@gmail.com>
2024-05-28 20:45:27 -0700

    Upvote/Downvote FEATURE

--- frontend/components/cards/post.tsx
+++ frontend/components/cards/post.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { Box, Card, Flex, Separator, Text } from '@radix-ui/themes';
 import {
   ThickArrowUpIcon,
@@ -29,80 +30,117 @@ export default function PostCard({
   likes,
   comments,
   datePosted,
+  isLiked = false,
+  isDisliked = false,
 }: Post) {
+  const [likeStatus, setLikeStatus] = useState<{
+    isLiked: boolean;
+    isDisliked: boolean;
+  }>({ isLiked, isDisliked });
+
+  const [likeCount, setLikeCount] = useState(likes);
+
+  const handleUpvote = () => {
+    if (likeStatus.isLiked) {
+      setLikeStatus({ isLiked: false, isDisliked: false });
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeStatus({ isLiked: true, isDisliked: false });
+      setLikeCount(likeStatus.isDisliked ? likeCount + 2 : likeCount + 1);
+    }
+  };
+
+  const handleDownvote = () => {
+    if (likeStatus.isDisliked) {
+      setLikeStatus({ isLiked: false, isDisliked: false });
+      setLikeCount(likeCount + 1);
+    } else {
+      setLikeStatus({ isLiked: false, isDisliked: true });
+      setLikeCount(likeStatus.isLiked ? likeCount - 2 : likeCount - 1);
+    }
+  };
+
   return (
-    <Box width='500px'>
-      <Card size='2'>
-        <Flex id='left' direction='column' justify='start' gap='2'>
-          <Flex id='user' direction='row' justify='between'>
-            <Text color='gray' size='2'>
+    <Box width="500px">
+      <Card size="2">
+        <Flex id="left" direction="column" justify="start" gap="2">
+          <Flex id="user" direction="row" justify="between">
+            <Text color="gray" size="2">
               Posted by: {user}
             </Text>
-            <Text color='gray' size='2'>
+            <Text color="gray" size="2">
               {datePosted.toLocaleTimeString()}
             </Text>
           </Flex>
-          <Flex id='content' direction='column' gap='1'>
-            <Text as='label' weight='bold' size='3' truncate>
+          <Flex id="content" direction="column" gap="1">
+            <Text as="label" weight="bold" size="3" truncate>
               {title}
             </Text>
-            <Flex height='60px' maxHeight='60px'>
+            <Flex height="60px" maxHeight="60px">
               <Text
-                as='p'
-                size='2'
+                as="p"
+                size="2"
                 style={{
                   overflow: 'hidden',
                   textOverflow: 'ellipsis',
-                  whiteSpace: 'pretty',
+                  whiteSpace: 'nowrap',
                 }}
               >
                 {description}
               </Text>
             </Flex>
           </Flex>
-          <Separator orientation='horizontal' size='4' />
+          <Separator orientation="horizontal" size="4" />
           <Flex
-            id='controls'
-            direction='row'
-            justify='start'
-            align='center'
-            gap='3'
+            id="controls"
+            direction="row"
+            justify="start"
+            align="center"
+            gap="3"
           >
             <Box
-              id='likes'
+              id="likes"
               style={{
                 background: 'var(--gray-a3)',
                 borderRadius: 'var(--radius-3)',
               }}
-              className='py-1 px-2'
+              className="py-1 px-2"
             >
-              <Flex gap='3' justify='start' align='center'>
-                <Flex direction='row' justify='start' align='center' gap='1'>
-                  <ThickArrowUpIcon className='hover:cursor-pointer icon-hover' />
-                  <Text as='label' size='2'>
-                    {likes}
+              <Flex gap="3" justify="start" align="center">
+                <Flex direction="row" justify="start" align="center" gap="1">
+                  <ThickArrowUpIcon
+                    className={`hover:cursor-pointer icon-hover ${likeStatus.isLiked ? 'liked' : ''
+                      }`}
+                    onClick={handleUpvote}
+                  />
+                  <Text as="label" size="2">
+                    {likeCount}
                   </Text>
                 </Flex>
-                <ThickArrowDownIcon className='hover:cursor-pointer icon-hover' />
+                <ThickArrowDownIcon
+                  className={`hover:cursor-pointer icon-hover ${likeStatus.isDisliked ? 'disliked' : ''
+                    }`}
+                  onClick={handleDownvote}
+                />
               </Flex>
             </Box>
             <Box
-              id='likes'
+              id="comments"
               style={{
                 background: 'var(--gray-a3)',
                 borderRadius: 'var(--radius-3)',
               }}
-              className='py-1 px-2 hover:cursor-pointe'
+              className="py-1 px-2"
             >
               <Flex
-                gap='2'
-                direction='row'
-                justify='start'
-                align='center'
-                className='hover:cursor-pointer'
+                gap="2"
+                direction="row"
+                justify="start"
+                align="center"
+                className="hover:cursor-pointer"
               >
-                <ChatBubbleIcon className='hover:cursor-pointer' />
-                <Text as='label' size='2' className='hover:cursor-pointer'>
+                <ChatBubbleIcon className="hover:cursor-pointer" />
+                <Text as="label" size="2" className="hover:cursor-pointer">
                   {comments}
                 </Text>
               </Flex>

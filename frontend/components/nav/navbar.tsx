3bdc18f7a85638b40fc0bec04a0585c03fb43306
Edwin J <35356243+pristine@users.noreply.github.com>
2024-05-29 17:19:18 -0700

    fix(nav): center nav bar + z-index

--- frontend/components/nav/navbar.tsx
+++ frontend/components/nav/navbar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
       top='0'
       left='0'
       right='0'
-      className='w-full transition-all border-b border-[#2b2b2b] backdrop-blur-xl'
+      className='w-full transition-all border-b border-[#2b2b2b] backdrop-blur-xl z-40'
     >
       <Flex
         justify='between'
@@ -24,43 +24,18 @@ export default function NavBar() {
         height='50px'
       >
         <Box id='logo'></Box>
-        <Nav />
+
+        <Box
+          style={{
+            position: 'absolute',
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        >
+          <Nav />
+        </Box>
 
         <Box id='user'>
-          {/* <NavigationMenu.Root>
-            <NavigationMenu.List>
-              <NavigationMenu.Item>
-                <NavigationMenu.Trigger className='hover:cursor-pointer flex items-center'>
-                  <Avatar className='w-8 h-8'>
-                    <AvatarImage src='/path/to/profile-pic.jpg' alt='Profile Picture' />
-                    <AvatarFallback>BC</AvatarFallback>
-                  </Avatar>
-                  <Text size='2' className='ml-2'>Name</Text>
-                  <CaretDownIcon
-                    className='ml-1 relative top-[1px] transition-transform duration-[250] ease-in'
-                    aria-hidden
-                  />
-                </NavigationMenu.Trigger>
-                <NavigationMenu.Content>
-                  <Flex className='flex-col gap-2 p-2'>
-                    <NavigationMenu.Link href='/profile'>
-                      <Text>Profile</Text>
-                    </NavigationMenu.Link>
-                    <NavigationMenu.Link href='/settings'>
-                      <Text>Settings</Text>
-                    </NavigationMenu.Link>
-                    <NavigationMenu.Link href='/logout'>
-                      <Text>Logout</Text>
-                    </NavigationMenu.Link>
-                  </Flex>
-                </NavigationMenu.Content>
-              </NavigationMenu.Item>
-            </NavigationMenu.List>
-
-            <NavigationMenu.Indicator />
-          </NavigationMenu.Root> 
-          */}
-
           <Flex className='flex flex-row gap-2 ml-auto'>
             <NextLink
               href='/'
@@ -79,22 +54,14 @@ export default function NavBar() {
                 </Flex>
               </HoverCard.Trigger>
               <HoverCard.Content>
-                <Flex direction="column">
+                <Flex direction='column'>
                   <Text>Settings</Text>
                   <Text>Change Lighting</Text>
                 </Flex>
               </HoverCard.Content>
             </HoverCard.Root>
           </Flex>
-
-
-
-
-
-
-
         </Box>
-
       </Flex>
     </Box>
   );
